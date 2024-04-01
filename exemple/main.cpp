27218c47d9a45c801c105a81fa6cf1b8363125ff
Nicolas Fourgheon <boby15000@hotmail.com>
2024-04-01 15:32:10 +0400

    Ajout du fichier exemple

---
+++ exemple/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <sensorACS.h>
+
+// DEFINITION DES PINS
+byte PIN_CAPTEUR_COURANT = A0;
+byte PIN_CAPTEUR_REF_TENSION = A1;
+byte SENSIBILITE_CAPT = 100;
+byte FREQUENCE = 50;
+
+sensorACS ACS712(SENSIBILITE_CAPT, PIN_CAPTEUR_COURANT, FREQUENCE, PIN_CAPTEUR_REF_TENSION );
+
+void setup() {
+  Serial.begin(9600);
+  ACS712.SetTension(230.0);
+}
+
+void loop() {
+  // Lecture du l'entrée Analogique
+  //GetValueADC(nbrLecture);
+  ACS712.ReadSensorADC();
+  
+  Serial.print("Courant Max : ");
+  Serial.print(ACS712.GetCourantCrete());
+  Serial.println("A");
+  Serial.print("Courant Eff : ");
+  Serial.print(ACS712.GetCourantEff());
+  Serial.println("A");/*
+  Serial.print(MargeErreur());
+  Serial.println("%");
+  Serial.println("-----------------");*/
+
+  delay(2000);
+}
